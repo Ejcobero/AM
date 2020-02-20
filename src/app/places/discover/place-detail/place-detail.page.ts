0f0a32083abdfda636ecde33c43c0c51a44bf9ac
Ejcobero <ejcobero@gmail.com>
2020-02-20 14:06:49 +0800

    created a modal for booking

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
+import { Place } from '../../place.model';
 
 @Component({
   selector: 'app-place-detail',
@@ -8,15 +10,21 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  place: Place;
 
-  constructor(private router: Router, private navCTRL: NavController) { }
+  constructor(private router: Router, private navCTRL: NavController, private modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
 
   onBookPlace() {
     // this.router.navigateByUrl('/places/tabs/discover');
-    this.navCTRL.navigateBack('/places/tabs/discover');
+    // this.navCTRL.navigateBack('/places/tabs/discover');
     // this.navCTRL.pop();
+    this.modalCtrl.create({component: CreateBookingComponent,
+       componentProps: {selectedPlace: this.place}
+      }).then(modalEl => {
+      modalEl.present();
+    });
   }
 }
